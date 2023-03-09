2869a1a2c8ff0404655ba7f0a6ef02d1a2744555
Bohan Zhang <920060768@qq.com>
2023-03-09 22:03:18 +1100

    Index Page Design Finished

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,4 +9,9 @@ export function render() {
     common.clearPage()
     document.body.appendChild(common.template("index-template"))
     common.active("nav-home")
+    document.getElementById("like").addEventListener("click", changeImage)
+}
+
+function changeImage() {
+    document.getElementById("like-image").src = "assets/liked.svg"
 }
